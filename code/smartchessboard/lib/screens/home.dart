0780a7537e7fe332d23e332df3e3f21cace12b87
Sahira100 <116058976+Sahira100@users.noreply.github.com>
2024-01-27 20:47:21 +0530

    fix001

--- code/smartchessboard/lib/screens/home.dart
+++ code/smartchessboard/lib/screens/home.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:smartchessboard/screens/login.dart';
-// import 'package:smartchessboard/screens/mainmenu.dart';
 import 'package:smartchessboard/screens/signup.dart';
-import 'package:smartchessboard/screens/user_guide.dart';
 
 class HomePage extends StatelessWidget {
   static String routeName = '/home';
@@ -15,14 +13,11 @@ class HomePage extends StatelessWidget {
     Navigator.pushNamed(context, SignupPage.routeName);
   }
 
-  void GoMainMenu(BuildContext context) {
-    //Navigator.pushNamed(context, SignupPage.routeName);
-    // Navigator.pushNamed(context, MainMenu.routeName);
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
       body: SafeArea(
         child: Container(
           // we will give media query height
@@ -44,95 +39,85 @@ class HomePage extends StatelessWidget {
                     style: TextStyle(
                       fontWeight: FontWeight.bold,
                       fontSize: 30,
+
                     ),
+                    
                   ),
                   SizedBox(
                     height: 20,
                   ),
-                  Text(
-                    "Experience live chess moves on a real board\nConnect, play, and enjoy the game",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Colors.grey[700],
-                      fontSize: 15,
-                    ),
-                  )
+                  Text("Experience live chess moves on a real board\nConnect, play, and enjoy the game",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.grey[700],
+                    fontSize: 15,
+
+                  ),)
                 ],
               ),
               Container(
                 height: MediaQuery.of(context).size.height / 2,
                 decoration: BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage("assets/welcome.png"))),
+                  image: DecorationImage(
+                    image: AssetImage("assets/welcome.png")
+                  )
+                ),
               ),
+
               Column(
                 children: <Widget>[
                   // the login button
-                  // MaterialButton(
-                  //   minWidth:250,
-                  //   height: 60,
-                  //   onPressed: () {
-                  //     //LogIn(context);
-                  //     GoMainMenu(context);
+                  MaterialButton(
+                    minWidth:250,
+                    height: 60,
+                    onPressed: () {
+                      LogIn(context);
 
-                  //   },
-                  //   // defining the shape
-                  //   shape: RoundedRectangleBorder(
-                  //     side: BorderSide(
-                  //       color: Colors.black
-                  //     ),
-                  //     borderRadius: BorderRadius.circular(50)
-                  //   ),
-                  //   child: Text(
-                  //     "Login",
-                  //     style: TextStyle(
-                  //       fontWeight: FontWeight.w300,
-                  //       fontSize: 15
-                  //     ),
-                  //   ),
-                  // ),
+                    },
+                    // defining the shape
+                    shape: RoundedRectangleBorder(
+                      side: BorderSide(
+                        color: Colors.black
+                      ),
+                      borderRadius: BorderRadius.circular(50)
+                    ),
+                    child: Text(
+                      "Login",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w300,
+                        fontSize: 15
+                      ),
+                    ),
+                  ),
                   // creating the signup button
-                  SizedBox(height: 10),
+                  SizedBox(height:20),
                   MaterialButton(
                     minWidth: 250,
                     height: 60,
-                    onPressed: () {
-                      //SingUp(context);
-                      GoMainMenu(context);
+                    onPressed: (){
+                      SingUp(context);
+
                     },
                     color: Color(0xff0095FF),
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(50),
+                      
                     ),
                     child: Text(
-                      "Continue",
+                      "Sign up",
                       style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w300,
-                          fontSize: 15),
+                        color: Colors.white,
+                        fontWeight: FontWeight.w300,
+                        fontSize: 15
+                      ),
                     ),
-                  ),
+                  )
 
-                  Container(height: 10),
-                  // Add User Guide button
-                  // TextButton(
-                  //   onPressed: () {
-                  //     Navigator.of(context)
-                  //         .push(MaterialPageRoute(builder: (_) {
-                  //       return UserGuide();
-                  //     }));
-                  //   },
-                  //   child: Text(
-                  //     "User Guide",
-                  //     style: TextStyle(
-                  //       color: Colors.blue,
-                  //       fontWeight: FontWeight.w300,
-                  //       fontSize: 15,
-                  //     ),
-                  //   ),
-                  // ),
                 ],
               )
+
+
+
             ],
           ),
         ),
